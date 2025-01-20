59196870b8df7888e7601426007d7db01119cc93
Owen Gaydosz <owgydz@proton.me>
2025-01-19 23:11:04 -0500

    osmiumConfig: Create osmium.py to fetch Osmium

---
+++ fetch_configs/osmium.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025 Owen Gaydosz. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import ast
+import sys
+
+import config_util
+
+
+class OsmiumBrowser(config_util.Config):
+    @staticmethod
+    def fetch_spec(props):
+        url = 'https://github.com/owgydz/osmium.git'
+      
+        solution = {
+            'name': 'src',
+            'url': url,
+            'managed': False,
+            'custom_deps': {},
+            'custom_vars': {},
+        }
+
+        spec = {
+            'solutions': [solution],
+        }
+
+        return {
+            'type': 'gclient_git',
+            'gclient_git_spec': spec,
+        }
+
+    @staticmethod
+    def expected_root(props):
+        return 'src'
+
+
+def main(argv=None):
+    return OsmiumBrowser().handle_args(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
